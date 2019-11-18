51a723e598ef49a60c9a46586f5ea77431d2682c
Zhen404 <zl2632@columbia.edu>
2019-11-17 22:23:46 -0500

    refactor

---
+++ go_chat/message.go
@@ -0,0 +1,11 @@
+package main
+
+type Message struct {
+	UserName string `json: "UserName"`
+	Body string `json:"body"`
+	Timestamp string `json:"timestamp"`
+}
+
+func (self *Message) String() string {
+	return self.UserName + " at " + self.Timestamp + " says " + self.Body
+}
